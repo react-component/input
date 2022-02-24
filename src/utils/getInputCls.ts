d113b46cb80ad293fb27b92a100068390e7a1ec0
MadCcc <1075746765@qq.com>
2022-02-24 19:17:14 +0800

    chore: code clean

--- src/utils/getInputCls.ts
+++
@@ -1,9 +0,0 @@
-import classNames from 'classnames';
-
-const getInputCls = (prefixCls?: string, disabled?: boolean) => {
-  return classNames(prefixCls, {
-    [`${prefixCls}-disabled`]: disabled,
-  });
-};
-
-export default getInputCls;
