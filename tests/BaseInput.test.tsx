f556a8f1952abba58c17f853c710c4456e785f7e
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-10-24 15:36:49 +0800

    fix(): :bug: 点击prefix或suffix无法获取焦点 (#24)
    * fix(): :bug: 点击prefix或suffix无法获取焦点
    
    * fix(): :bug: 点击prefix或suffix无法获取焦点

--- tests/BaseInput.test.tsx
+++ tests/BaseInput.test.tsx
@@ -126,7 +126,7 @@ describe('BaseInput', () => {
         prefix="$"
       />,
     );
-    fireEvent.mouseDown(container.querySelector('.rc-input-affix-wrapper')!);
+    fireEvent.click(container.querySelector('.rc-input-affix-wrapper')!);
     expect(document.activeElement).toBe(container.querySelector('input'));
   });
 });
