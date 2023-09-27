b268c698af62cd1727287ae9d92ac336bdec20df
二货机器人 <smith3816@gmail.com>
2023-09-27 14:17:12 +0800

    test: add test case

--- tests/count.test.tsx
+++ tests/count.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import Input from '../src';
 
@@ -24,5 +24,57 @@ describe('Input.Count', () => {
     expect(
       container.querySelector('.rc-input-show-count-suffix')?.textContent,
     ).toEqual('1');
+  });
+
+  it('exceed style', () => {
+    const { container } = render(
+      <Input count={{ show: true, max: 5 }} value="👨‍👩‍👧‍👦" />,
+    );
+    expect(container.querySelector('.rc-input-out-of-range')).toBeTruthy();
+  });
+
+  it('show formatter', () => {
+    const { container } = render(
+      <Input
+        count={{
+          show: ({ value, count, maxLength }) =>
+            `${value}_${count}_${maxLength}`,
+        }}
+        value="👨‍👩‍👧‍👦"
+        maxLength={5}
+      />,
+    );
+    expect(
+      container.querySelector('.rc-input-show-count-suffix')?.textContent,
+    ).toEqual('👨‍👩‍👧‍👦_11_5');
+  });
+
+  it('exceedFormatter', () => {
+    const { container } = render(
+      <Input
+        count={{
+          show: true,
+          max: 3,
+          exceedFormatter: (val, { max }) =>
+            getSegments(val)
+              .filter((seg) => seg.index + seg.segment.length <= max)
+              .map((seg) => seg.segment)
+              .join(''),
+        }}
+      />,
+    );
+
+    // Allow input
+    fireEvent.compositionStart(container.querySelector('input')!);
+    fireEvent.change(container.querySelector('input')!, {
+      target: {
+        value: '🔥🔥🔥🔥🔥',
+      },
+    });
+    expect(container.querySelector('input')?.value).toEqual('🔥🔥🔥🔥🔥');
+
+    // Fallback
+    fireEvent.compositionEnd(container.querySelector('input')!);
+    expect(container.querySelector('input')?.value).toEqual('🔥');
   });
 });
