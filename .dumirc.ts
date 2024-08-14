7da4043560683b0c8526479acf425f6842df2f82
Muzzle886 <2568682437@qq.com>
2024-08-14 17:40:43 +0800

    docs: missing basePath

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'dumi';
 
+const basePath = process.env.GH_PAGES ? '/input/' : '/';
+const publicPath = process.env.GH_PAGES ? '/input/' : '/';
+
 export default defineConfig({
   mfsu: false,
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
@@ -8,4 +11,6 @@ export default defineConfig({
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
   outputPath: '.doc',
+  base: basePath,
+  publicPath,
 });
