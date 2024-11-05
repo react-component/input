963ab6ca47cd30dd2e0cffdc63b7faa4aee83480
r0mm4k <r0mmm4k@gmail.com>
2024-11-05 13:06:28 +0300

    Refactoring.

--- src/Input.tsx
+++ src/Input.tsx
@@ -94,7 +94,10 @@ const Input = forwardRef<InputRef, InputProps>((props, ref) => {
   }));
 
   useEffect(() => {
-    keyLockRef.current && (keyLockRef.current = false);
+    if (keyLockRef.current) {
+      keyLockRef.current = false;
+    }
+
     setFocused((prev) => (prev && disabled ? false : prev));
   }, [disabled]);
 
