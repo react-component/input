d552c8a7672685fb0d31fd64adec4633944aa898
高艳兵 <yanbing.gao@aliyun.com>
2026-05-22 14:11:04 +0800

    fix: expose input internal types

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import BaseInput from './BaseInput';
 import Input from './Input';
 import TextArea from './TextArea';
+import type { HolderRef } from './BaseInput';
 
 export type {
   AutoSizeType,
+  BaseInputProps,
   InputProps,
   InputRef,
   ResizableTextAreaRef,
   TextAreaProps,
   TextAreaRef,
 } from './interface';
+export type { HolderRef };
 
 export { default as ResizableTextArea } from './ResizableTextArea';
 
